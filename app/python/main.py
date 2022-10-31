ec2adde0b9e6b3cccb76c75c4f6ec626aff17190
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-11-01 01:07:13 +0700

    Sleep 5sec for testing

--- app/python/main.py
+++ app/python/main.py
@@ -1,13 +1,17 @@
 import os
+import time
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
+    time.sleep(5)
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
