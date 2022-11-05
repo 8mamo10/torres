05ce5692ab8813e9232f6677b1fe764de77ca0f3
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-11-05 23:51:36 +0700

    Add function to do heavy task

--- app/python/main.py
+++ app/python/main.py
@@ -1,14 +1,21 @@
 import os
 import time
+import logging
 
 from flask import Flask
 
 app = Flask(__name__)
 
 
+def some_heavy_task():
+    logging.warning("Start heavy task!!")
+    time.sleep(5)
+    logging.warning("Finish heavy task!!")
+
+
 @app.route("/")
 def hello_world():
-    time.sleep(5)
+    some_heavy_task()
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
 
